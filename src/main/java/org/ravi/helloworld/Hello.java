4568350957f53f85a4d1cb35ce6c25192c25431a
glam <rajib.kol@gmail.com>
2018-01-07 13:55:08 +0530

    change for sonar analysis 1

--- src/main/java/org/ravi/helloworld/Hello.java
+++ src/main/java/org/ravi/helloworld/Hello.java
@@ -2,9 +2,9 @@ package org.ravi.helloworld;
 
 public class Hello {
 	
-	public String displayHelloMessage(String message){
-		message = "hi";
-		return message;
+	public String displayHelloMessage(String message1){
+		message1 = "hi";
+		return message1;
 	}
 
 }
